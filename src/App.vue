3b2f4dfea09be8b537301c638625aad564c57df2
Gapabhd14 <gapa.bhd14@gmail.com>
2023-06-14 15:15:53 +0300

    14/6

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
   <div id="app">
-        <HelloWorld msg="You did it!" />
-        <h1>hh</h1>
+	<open-screen v-if="showcomponent === 0" @showComp="()=>{this.showcomponent++}"></open-screen>
+	  <!-- <three-d/> -->
   </div>
 </template>
 
-<style scoped>
+<script >
+import OpenScreen from '@/components/OpenScreen.vue';
+// import threeD from './components/threeD.vue';
+export default {
+	name: 'app',
+	data() {
+		return {
+		showcomponent: 0,
+		};
+	},
+	components: {
+		OpenScreen,
+	},
   
+};
+</script>
+
+
+<style scoped>
+#app {
+	width: 100%;
+	height: 100%;
+	direction: rtl;
+	overflow: hidden;
+	margin: 0%;
+}
+@font-face {
+	font-family: varela;
+	src: url(./assets/fonts/Varela_Round/VarelaRound-Regular.ttf);
+}
+@font-face {
+	font-family: horev;
+	src: url(./assets/fonts/horev/horevclm-heavy-webfont.ttf);
+}
+* {
+	font-family: varela;
+	overflow: hidden;
+	background-color: rgb(217, 217, 217);
+	background-size: 100% 100%;
+}
 </style>
---
+++ src/components/OpenScreen.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="open-screen" v-touch:swipe="swipeHandler">
+       <p class="titlePuma">פומה</p>
+	   <div id="startBut"  @click="() => { this.$emit('showComp', 1); }">
+			<p id="textStart">שנתחיל?</p>
+	   </div>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vue2TouchEvents from 'vue2-touch-events'
+import { ModelObj } from 'vue-3d-model';
+
+Vue.use(Vue2TouchEvents)
+export default {
+	name: 'open-screen',
+	data() {
+		return {
+			page: 0,
+		};
+	},
+    // mounted() {
+        
+	// },
+	methods: {
+		swipeHandler (direction) {
+            if(direction == "top"){
+				console.log("yay"); // נעבור לקומפוננטה של הסבר על הלומדה
+			}
+        }
+	}
+};
+</script>
+
+<style scoped>
+#open-screen {
+	width: 100vw;
+	height: 100vh;
+}
+#startBut{
+	width: 25vw;
+	height: 7vh;
+	top: 45vh;
+	right: 33vw;
+	background-color: rgb(237, 125, 49);
+	border-radius: 1vh;
+	box-shadow: 0 20px 20px rgb(0 0 0 / 0.2);
+}
+#textStart{
+	font-size:2vh;
+	text-align: center;
+	padding:2vh ;
+}
+.titlePuma {
+	color: rgb(237, 125, 49);
+	font-size: 40vw;
+	font-family: horev;
+	position: absolute;
+	right: 25vw;
+	animation: titleAnimation 1s 1 forwards;
+}
+@keyframes titleAnimation {
+	from {top: 0vh; opacity: 0.1;}
+	to {top: 20vh; opacity: 1;}
+}
+.model {
+	background-color: aqua;
+}
+</style>
